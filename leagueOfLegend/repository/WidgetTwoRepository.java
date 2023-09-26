ec0c949769a7c1cf608f235705c7da2811786de2
이상훈 <132039559+Hoonscucci@users.noreply.github.com>
2023-09-26 14:15:04 +0900

    Delete leagueOfLegend directory

--- leagueOfLegend/repository/WidgetTwoRepository.java
+++
@@ -1,9 +0,0 @@
-package project.leagueOfLegend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.leagueOfLegend.entity.WidgetTwo;
-
-@Repository
-public interface WidgetTwoRepository extends JpaRepository<WidgetTwo, Integer> {
-}
